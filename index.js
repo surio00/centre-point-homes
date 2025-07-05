38de7667af30a6fe50f4f074b2f52f342be22e72
surio00 <uudesai2009@gmail.com>
2025-07-05 20:38:19 +0530

    Fixed the issue with vercel build

---
+++ index.js
@@ -0,0 +1,3 @@
+// This file helps Vercel recognize this as a Node.js project
+// The actual website is served from the dist/public directory
+console.log('Centre Point Homes - Built with Vite + React');
