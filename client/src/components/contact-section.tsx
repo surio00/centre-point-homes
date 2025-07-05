c5ef318bf570613b265e8c3032f79e2cd603b386
surio00 <uudesai2009@gmail.com>
2025-07-05 21:43:26 +0530

    added formspree and removed deployment

--- client/src/components/contact-section.tsx
+++ client/src/components/contact-section.tsx
@@ -64,7 +64,7 @@ export default function ContactSection() {
     setIsSubmitting(true);
     
     try {
-      const response = await fetch('https://formspree.io/f/YOUR_FORM_ID', {
+      const response = await fetch('https://formspree.io/f/xdkzvgya', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
